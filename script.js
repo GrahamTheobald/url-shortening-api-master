3596deccea63ee7632e1c72e0a7419a799781ed4
Graham Theobald <Graham_Theobald@live.co.uk>
2022-11-24 18:02:01 +0000

    toggle menu mobile

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const resultTemplate = document.querySelector('#result_div')
 const input = document.querySelector('[data-input]')
 const inputWrapper = input.closest('.input__url-wrapper')
 const submit = document.querySelector('[data-submit]')
+const menuButton = document.querySelector('.nav__toggle-menu')
+const menu = document.querySelector('.nav__containers')
 
 const URL = `https://api.shrtco.de/v2/shorten?url=`
 const LOCAL_STORAGE_KEY = 'url-shortening-api-'
@@ -9,7 +11,10 @@ const LOCAL_STORAGE_KEY = 'url-shortening-api-'
 submit.addEventListener('click', () => {
 	shortenLink(input.value)
 })
+
 input.addEventListener('input', removeInvalid)
+
+menuButton.addEventListener('click', toggleMenu)
 
 async function shortenLink(link) {
 	removeInvalid()
@@ -29,4 +34,9 @@ async function apiCall(link) {
 function removeInvalid() {
 	inputWrapper.classList.remove('invalid')
 }
+
+function toggleMenu() {
+	menu.classList.toggle('reveal')
+}
+
 const resultClone = resultTemplate.content.firstElementChild.cloneNode(true)
